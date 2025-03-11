e165574868ede2383bc3fe76232e0f024d69d881
AssabTigle <132921812+AssabTigleG@users.noreply.github.com>
2025-03-11 20:21:43 +0500

    Accepts List of Orders | Automatic Price Calculations

--- models/Orders.model.js
+++ models/Orders.model.js
@@ -1,19 +1,33 @@
 const mongoose = require("mongoose");
 
+const ProductItemSchema = new mongoose.Schema({
+  product_name: { type: String, required: true },
+  product_category: { type: String, required: true },
+  product_quantity: { type: Number, required: true },
+  product_price: { type: Number, required: true },
+});
+
 const OrdersSchema = mongoose.Schema(
   {
     business_name: { type: String },
     location: { type: String },
     phone_num: { type: Number },
     invoice_id: { type: String, required: true, unique: true },
-    product_name: [{ type: String, required: true }],
-    product_category: { type: String, required: true },
-    product_quantity: { type: Number, required: true },
-    product_price: { type: Number, required: true },
+    products: { type: [ProductItemSchema], required: true },
     payment_method: { type: String, enum: ["Cash", "Card", "Online Payment"] },
+    // Total price will be calculated automatically
     total_price: { type: Number, required: true },
   },
   { timestamps: true }
 );
+
+// Calculate total_price before saving the order
+OrdersSchema.pre("save", function (next) {
+  this.total_price = this.products.reduce(
+    (total, product) => total + product.product_price * product.product_quantity,
+    0
+  );
+  next();
+});
 
 module.exports = mongoose.model("Orders", OrdersSchema);
