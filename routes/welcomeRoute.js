2b8a3fd258a5abaa9616dd3553de983390c96ea5
AssabTigle <132921812+AssabTigleG@users.noreply.github.com>
2025-05-06 09:34:24 -0700

    Removed unnecessary controller class import

--- routes/welcomeRoute.js
+++ routes/welcomeRoute.js
@@ -1,9 +1,11 @@
-const {welcomeController} = require("../controllers/welcomeController")
-const express = require("express")
-const router = express.Router()
+// File: subhanTraders-app/routes/welcomeRoute.js
+const express = require("express");
+const router = express.Router();
 
+const welcomeController = (req, res) => {
+    res.status(200).json({ message: "Welcome to the SubhanTraders API v1 Homepage!" });
+};
 
-router.get("/homepage", welcomeController)
-
+router.get("/homepage", welcomeController);
 
 module.exports = router;
