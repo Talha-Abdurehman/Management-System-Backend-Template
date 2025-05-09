cff70c863083f9b12ab0aaf6615fcbc49098ddbf
Talha-Abdurehman <talhakyle124@gmail.com>
2025-05-09 11:23:42 -0700

    Added isAdmin Attribute

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,14 +7,14 @@ dotenv.config();
 
 exports.createUser = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password, isAdmin } = req.body;
 
     const existingUser = await User.findOne({ username });
     if (existingUser)
       return res.status(400).json({ message: "User Already Exists" });
 
     const hashedPass = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, password: hashedPass });
+    const newUser = new User({ username, password: hashedPass, isAdmin });
     await newUser.save();
     res.status(201).json({ message: "User Created Successfully" });
   } catch (e) {
