6087ccb788e2605c4f4edccf63a10ed52c2703e8
Talha-Abdurehman <talhakyle124@gmail.com>
2025-05-16 09:20:44 -0700

    Added a new Attribute to Items Schema: isReturned

--- models/Items.model.js
+++ models/Items.model.js
@@ -4,10 +4,20 @@ const ItemSchema = new mongoose.Schema(
   {
     name: { type: String, unique: true, required: true },
     description: { type: String },
-    price: { type: Number, required: true, min: [0, 'Price cannot be negative'] },
-    quantity: { type: Number, required: true, min: [0, 'Quantity cannot be negative'], default: 0 },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price cannot be negative"],
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Quantity cannot be negative"],
+      default: 0,
+    },
     category: { type: String, required: true },
     imageUrl: { type: String },
+    isReturned: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
